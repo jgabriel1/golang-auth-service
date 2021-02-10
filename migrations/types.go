22daf59173a98a044d8365212cde3d527f2f1f6f
jgabriel1 <josegabriel@det.ufc.br>
2021-02-10 20:26:00 +0000

    Deleted all of the old files, they won't be needed anymore.

--- migrations/types.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Migration interface {
-	Up()
-	Down()
-}
--- src/services/types.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import (
-	"golang-auth-service/src/models"
-)
-
-type IRegisterUser interface {
-	Execute(username, password string) *models.User
-}
