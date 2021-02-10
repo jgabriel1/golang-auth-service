9532544cf045837883e3b97bea2afeae18df2b44
jgabriel1 <josegabriel@det.ufc.br>
2021-02-10 20:29:17 +0000

    Created user model and repository interface.

---
+++ domain/model/user.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"auth-service/domain/dtype"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	PasswordHash []byte    `json:"password"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type IUsersRepository interface {
+	Register(user *User) error
+	FindByEmail(email *dtype.Email) (*User, error)
+	FindByUserName(username string) (*User, error)
+}
+
+func NewUser(name string, email *dtype.Email, passwordHash *dtype.PasswordHash) (*User, error) {
+	user := &User{
+		Name:         name,
+		Email:        email.Value,
+		PasswordHash: passwordHash.Value,
+	}
+
+	user.Id = uuid.New()
+	user.CreatedAt = time.Now()
+
+	err := user.IsValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *User) IsValid() error {
+	return nil
+}
